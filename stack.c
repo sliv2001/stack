f1ff509aa37ec9abc6f8fa8f3caaf6cac558e0b0
sliv2001 <ivan.sladkov@yandex.ru>
2020-11-19 11:55:08 +0300

    Added semaphores.

--- stack.c
+++ stack.c
@@ -1,11 +1,40 @@
 #include "stack.h"
 
+/*TODO добавить обработку сигналов */
+
 const size_t elemcount = 100000;
 
-int wrStack(stack_h s, void* addr) {
+int wrStack(stack_h s, struct stack_t* stack, void* addr) {
+	struct sembuf semb;
+	semb.sem_num = 0;
+	semb.sem_op = -1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return -1;
 	*((stack_h*)addr) = s;
+	semb.sem_num = 0;
+	semb.sem_op = 1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return 1;
 	return 0;
 };
+
+int rdStack(stack_h* s, struct stack_t* stack, void* addr) {
+	struct sembuf semb;
+	semb.sem_num = 0;
+	semb.sem_op = -1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return -1;
+	*s = *((stack_h*)addr);
+	semb.sem_num = 0;
+	semb.sem_op = 1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return 1;
+	return 0;
+}
 
 struct stack_t* attach_stack(key_t key, int size){
 	void* addr;
@@ -13,11 +42,17 @@ struct stack_t* attach_stack(key_t key, int size){
 	struct shmid_ds str;
 	struct stack_t* stack;
 	stack_h s;
+	int sem = semget(key, 1, IPC_CREAT|0666);
 	int shmem = shmget(key, elemcount*size+sizeof(stack_h), IPC_CREAT|0666);  //здесь можно поправить длину памяти
-	if (shmem==(-1)) {
+	if (shmem<0) {
 		return NULL;
 	}
+	if (sem<0)
+		return NULL;
 	stack = (struct stack_t*)malloc(sizeof(struct stack_t));
+	stack->shmem = shmem;
+	stack->sem = sem;
+	stack->addr = addr;
 	addr = shmat(shmem, 0, 0);
 	shmctl(shmem, IPC_STAT, &str);
 	if (str.shm_cpid!=getpid())
@@ -30,54 +65,59 @@ struct stack_t* attach_stack(key_t key, int size){
 		s.count = 0;
 		s.progs = 0;
 		s.mdestruct = 0;
-		wrStack(s, addr);
+		wrStack(s, stack, addr);
 	}
-	s = *((stack_h*)addr);
+	rdStack(&s, stack, addr);
 	s.progs++;
-	wrStack(s, addr);
+	wrStack(s, stack, addr);
 	if (!newStack&&size!=s.size){
 		detach_stack(addr);
 		return NULL;
 	}
-	stack->shmem = shmem;
-	stack->sem = 0;                   //поправить
-	stack->addr = addr;
 	return stack;
 }
 
 int detach_stack(struct stack_t* stack){
 	stack_h s;
-	s = *((stack_h*)stack->addr);
+	rdStack(&s, stack, stack->addr);
 	s.progs--;
-	wrStack(s, (void*)stack->addr);
-	if (s.mdestruct&&s.progs == 0)
+	wrStack(s, stack, (void*)stack->addr);
+	semctl(stack->sem, 0, IPC_RMID);
+	if (s.mdestruct&&s.progs == 0){
 		return shmctl(stack->shmem, IPC_RMID, NULL);
+	}
 	else
 		return shmdt(stack->addr);
 }
 
 int mark_destruct(struct stack_t* stack){
 	stack_h s;
-	s = *((stack_h*)stack->addr);
+	rdStack(&s, stack, stack->addr);
 	s.mdestruct = 1;
-	return wrStack(s, (void*)stack->addr);
+	return wrStack(s, stack, (void*)stack->addr);
 }
 
 int get_size(struct stack_t* stack){
 	stack_h s;
-	s = *((stack_h*)stack->addr);
+	rdStack(&s, stack, stack->addr);
 	return s.size*elemcount;
 }
 
 int get_count(struct stack_t* stack){
 	stack_h s;
-	s = *((stack_h*)stack->addr);
+	rdStack(&s, stack, stack->addr);
 	return s.count*s.size;
 }
 
 int push(struct stack_t* stack, void* val){
 	stack_h s;
 	int i;
+	struct sembuf semb;
+	semb.sem_num = 0;
+	semb.sem_op = -1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return -1;
 	s = *((stack_h*)stack->addr);
 	for (i=0; i<s.size; i++){
 		*((char*)stack->addr+s.top+i) = *((char*)val+i);
@@ -85,12 +125,23 @@ int push(struct stack_t* stack, void* val){
 	s.count++;
 	s.top+=s.size;
 	*((stack_h*)stack->addr) = s;
+	semb.sem_num = 0;
+	semb.sem_op = 1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return 1;
 	return 0;
 }
 
 int pop(struct stack_t* stack, void* val){
 	stack_h s;
 	int i;
+	struct sembuf semb;
+	semb.sem_num = 0;
+	semb.sem_op = -1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return -1;
 	s = *((stack_h*)stack->addr);
 	s.top-=s.size;
 	for (i=0; i<s.size; i++){
@@ -98,6 +149,11 @@ int pop(struct stack_t* stack, void* val){
 	}
 	s.count--;
 	*((stack_h*)stack->addr) = s;
+	semb.sem_num = 0;
+	semb.sem_op = 1;
+	semb.sem_flg = 0;
+	if (semop(stack->sem, &semb, 1)<0)
+		return 1;
 	return 0;
 }
 
